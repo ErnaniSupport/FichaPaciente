3d5dd7a38fd37dd506a3cd3d5917d570093412f4
ErnaniSupport <ernanireis@hotmail.com>
2022-12-03 12:40:10 -0300

    Ficha de Paciente

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+//const port = 5500;
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+//  res.end()
+});
+
+app.get("/meusDados", function(req, res){
+    res.end("Paciente: " + req.query.nome + "; Idade: " + req.query.idade + "; Nascimento: " + req.query.nascimento +  " ; Endereço: " + req.query.endereco + " ; Cidade: " + req.query.cidade + " ; Estado: " + req.query.estado + " ; Bairro: " + req.query.bairro + " ;" + req.query.texto);
+});
+
+app.listen(5500);
